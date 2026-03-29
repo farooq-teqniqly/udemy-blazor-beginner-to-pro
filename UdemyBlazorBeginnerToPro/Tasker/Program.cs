595f533c75889924d1e20e91e372e85281afe501
farooq-teqniqly <farooq@teqniqly.com>
2026-03-28 21:39:59 -0700

    Persist tasks to local storage

--- UdemyBlazorBeginnerToPro/Tasker/Program.cs
+++ UdemyBlazorBeginnerToPro/Tasker/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tasker.Components;
+using Tasker.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
@@ -10,5 +13,7 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
 });
+
+builder.Services.AddScoped<LocalStorageService>();
 
 await builder.Build().RunAsync();
---
+++ UdemyBlazorBeginnerToPro/Tasker/Services/LocalStorageService.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Microsoft.JSInterop;
+
+namespace Tasker.Services
+{
+    public sealed class LocalStorageService
+    {
+        private readonly IJSRuntime _js;
+
+        public LocalStorageService(IJSRuntime js)
+        {
+            ArgumentNullException.ThrowIfNull(js);
+
+            _js = js;
+        }
+
+        public async Task<T?> GetItemAsync<T>(string key)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(key);
+
+            var json = await _js.InvokeAsync<string>("localStorage.getItem", key);
+
+            return string.IsNullOrEmpty(json) ? default : JsonSerializer.Deserialize<T>(json);
+        }
+
+        public async Task SetItemAsync<T>(string key, T value)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(key);
+            ArgumentNullException.ThrowIfNull(value);
+
+            var json = JsonSerializer.Serialize(value);
+            await _js.InvokeVoidAsync("localStorage.setItem", key, json);
+        }
+    }
+}
