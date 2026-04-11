5ddda7b07f70b0ba79b84bc5b247c1599d75e4bb
teqniqly <teqniqly@teqniqly.com>
2026-04-11 14:41:42 -0700

    Add Loan models

---
+++ UdemyBlazorBeginnerToPro/LoanShark/Models/LoanInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoanShark.Models
+{
+    public sealed class LoanInputModel
+    {
+        [Required(ErrorMessage = "Loan amount is required.")]
+        [Range(100, 1000000, ErrorMessage = "Loan amount must be between $100 and $1,000,000.")]
+        public double Amount { get; set; }
+
+        [Required(ErrorMessage = "Interest rate is required.")]
+        [Range(0, 100, ErrorMessage = "Interest rate must be between 0% and 100%.")]
+        public double Rate { get; set; }
+
+        [Required(ErrorMessage = "Loan term in years is required.")]
+        [Range(1, 100, ErrorMessage = "Loan term must be between 1 and 100 years.")]
+        public int TermYears { get; set; }
+    }
+}
---
+++ UdemyBlazorBeginnerToPro/LoanShark/Models/LoanViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LoanShark.Models;
+
+public sealed class LoanViewModel
+{
+    public double MonthlyPayment { get; set; }
+    public double TotalCost { get; set; }
+    public double TotalInterest { get; set; }
+}
