2b582b35f1c9b6cb7a38bed918c8dacfda4e5665
teqniqly <teqniqly@teqniqly.com>
2026-04-19 12:13:36 -0700

    Add models for API response

---
+++ UdemyBlazorBeginnerToPro/NowPlayingApp/Models/MovieListResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NowPlayingApp.Models
+{
+    public class MovieListResponse
+    {
+        [JsonPropertyName("page")]
+        public int Page { get; set; }
+
+        [JsonPropertyName("results")]
+        public MovieResponse[] Results { get; set; } = [];
+
+        [JsonPropertyName("total_pages")]
+        public int TotalPages { get; set; }
+
+        [JsonPropertyName("total_results")]
+        public int TotalResults { get; set; }
+    }
+}
