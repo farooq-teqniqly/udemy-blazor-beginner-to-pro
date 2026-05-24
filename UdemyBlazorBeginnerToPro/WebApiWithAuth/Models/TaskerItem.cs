7056f036fdf1a0e5a6523077f30721a9ae8eca47
farooq-teqniqly <farooq@teqniqly.com>
2026-05-24 13:06:57 -0700

    Add TaskerItem and TaskerItemDto.

---
+++ UdemyBlazorBeginnerToPro/WebApiWithAuth/Models/TaskerItem.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApiWithAuth.Models
+{
+    public class TaskerItem
+    {
+        public bool Completed { get; set; } = false;
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Every task must have a name.")]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+        public virtual IdentityUser User { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+    }
+
+    public class TaskerItemDto
+    {
+        public bool Completed { get; set; } = false;
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Every task must have a name.")]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+    }
+}
